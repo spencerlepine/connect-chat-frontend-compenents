784b053de1ccb105f8a72c7c5d204dd5a27089b5
Spencer Lepine <spenlep@amazon.com>
2023-02-21 03:33:49 +0000

    auto code coverage comment

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   testEnvironment: 'jsdom',
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!src/**/*.d.ts', '!src/index.js'],
-  coverageReporters: ['json', 'lcov', 'text'],
+  coverageReporters: ['json', 'lcov', 'text', 'cobertura'],
   coverageThreshold: {
     global: {
       lines: 60
